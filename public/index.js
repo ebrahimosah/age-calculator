fe6963a21d7e40eb29a33afff1372a0733c1697f
Ibrahim <110994882+ebrahimosah@users.noreply.github.com>
2023-07-16 08:59:53 +0100

    current age functionality added && styles updated

--- public/index.js
+++ public/index.js
@@ -11,26 +11,31 @@ const yearMsg = document.querySelector(".year-error");
 const labelDay = document.querySelector(`label[for="day"]`);
 const labelMonth = document.querySelector(`label[for="month"]`);
 const labelYear = document.querySelector(`label[for="year"]`);
-let emptyFields = [];
-let invalidFields = [];
+let emptyFields = []; //error array
+// let invalidFields: string[] = [];
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) => {
     e.preventDefault();
     const dayValue = parseInt(dayInput.value);
     const monthValue = parseInt(monthInput.value);
     const yearValue = parseInt(yearInput.value);
+    //adds the error styles and message 
     function addError(label, day, msgDiv, msg) {
         label.classList.add('error-text');
         day.classList.add('error');
         msgDiv.textContent = msg;
     }
+    // removes the error styles and message
     function removeError(label, day, msgDiv) {
         label.classList.remove('error-text');
         day.classList.remove('error');
         msgDiv.textContent = "";
     }
+    //date validation
     function isValidDate(day, month, year) {
-        const currentDate = new Date();
+        const currentDate = new Date(); //today's date
         const currentYear = currentDate.getFullYear();
+        const date = new Date(year, month - 1, day); //inputted day
+        const birthDate = new Date(year, month, day);
         console.log(emptyFields);
         const isDayValid = day >= 1 && day <= 31;
         const isMonthValid = month >= 1 && month <= 12;
@@ -45,6 +50,26 @@ form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) 
             addError(labelYear, yearInput, yearMsg, 'must be in the past');
         }
         console.log('i ran');
+        if (isDayValid && isMonthValid && isYearValid && typeof year === 'number') {
+            if (date.getMonth() + 1 !== month || date.getDate() !== day) {
+                addError(labelDay, dayInput, dayMsg, 'invalid day');
+                console.log('added');
+            }
+        }
+        if (date.getMonth() + 1 === month && date.getDate() === day) {
+            const yearDiff = currentYear - birthDate.getFullYear();
+            const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+            const dayDiff = currentDate.getDate() - birthDate.getDate();
+            let ageYears = yearDiff;
+            let ageMonths = monthDiff;
+            let ageDays = dayDiff;
+            if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+                ageYears--;
+                ageMonths = 12 - Math.abs(monthDiff);
+                ageDays = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0).getDate() - Math.abs(dayDiff);
+            }
+            console.log(ageYears, ageMonths, ageDays);
+        }
     }
     //CHECKS IF EMPTY
     if (!dayValue) {
--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ const labelDay = document.querySelector(`label[for="day"]`) as HTMLLabelElement;
 const labelMonth = document.querySelector(`label[for="month"]`) as HTMLLabelElement;
 const labelYear = document.querySelector(`label[for="year"]`) as HTMLLabelElement;
 
-let emptyFields: string[] = [];
-let invalidFields: string[] = [];
+let emptyFields: string[] = []; //error array
+// let invalidFields: string[] = [];
 
 
 form?.addEventListener('submit',(e:Event)=>{
@@ -25,23 +25,32 @@ form?.addEventListener('submit',(e:Event)=>{
     const monthValue = parseInt(monthInput.value);
     const yearValue = parseInt(yearInput.value);
 
+    //adds the error styles and message 
     function addError(label:HTMLLabelElement, day:HTMLInputElement, msgDiv:HTMLSpanElement, msg:string ){
         label.classList.add('error-text');
         day.classList.add('error');
         msgDiv.textContent = msg;
     }
+    // removes the error styles and message
     function removeError(label:HTMLLabelElement, day:HTMLInputElement, msgDiv:HTMLSpanElement){
         label.classList.remove('error-text');
         day.classList.remove('error');
         msgDiv.textContent="";
     }
+
+    //date validation
     function isValidDate(day:number,month:number,year:number){
-        const currentDate = new Date();
+        const currentDate = new Date(); //today's date
         const currentYear = currentDate.getFullYear();
+        const date = new Date(year,month -1, day); //inputted day
         
+        const birthDate = new Date(year,month,day); 
+
+
+
+
         console.log(emptyFields);
-    
-        
+
 
        const isDayValid = day >=1 && day <= 31;
        const isMonthValid = month >=1 && month <=12;
@@ -60,8 +69,32 @@ form?.addEventListener('submit',(e:Event)=>{
         if(!isYearValid || !(typeof year === 'number')){
             addError(labelYear,yearInput,yearMsg,'must be in the past');
         }
-        console.log('i ran');
+
         
+        console.log('i ran');
+        if(isDayValid && isMonthValid && isYearValid && typeof year === 'number'){
+            if(date.getMonth() + 1 !== month || date.getDate() !== day){
+                addError(labelDay,dayInput,dayMsg,'invalid day');
+                console.log('added');
+            }
+        }
+        if(date.getMonth()+1 === month && date.getDate() === day){
+            const yearDiff = currentYear - birthDate.getFullYear();
+            const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+            const dayDiff = currentDate.getDate() - birthDate.getDate();
+    
+            let ageYears = yearDiff;
+            let ageMonths = monthDiff;
+            let ageDays = dayDiff;
+
+            if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)){
+                ageYears--;
+                ageMonths = 12 - Math.abs(monthDiff);
+                ageDays = new Date(currentDate.getFullYear(), currentDate.getMonth()-1,0).getDate()-Math.abs(dayDiff);
+            }
+
+           console.log(ageYears, ageMonths, ageDays);
+        }
     }
 
 //CHECKS IF EMPTY
@@ -97,7 +130,7 @@ form?.addEventListener('submit',(e:Event)=>{
         console.log('i got here');
         isValidDate(dayValue,monthValue,yearValue);
     }
-    
+
     
 
     //CLEARS EMPTY ARRAY
